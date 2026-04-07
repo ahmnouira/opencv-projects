26367187bad9a87a1278a72235d2f53e7672914f
Ahmed Nouira <ahmnouira@gmail.com>
2026-04-07 08:15:46 +0100

    style: format code and updates

--- simple_streaming.py
+++ simple_streaming.py
@@ -1,19 +1,20 @@
-import cv2 
-# create cap object      					        
-cap = cv2. VideoCapture(0)            	    
+import cv2
+
+# create cap object
+cap = cv2.VideoCapture(0)
 # check if the camera is opened
-if not cap.isOpened():                     	
-    print('Cannot open webcam')
+if not cap.isOpened():
+    print("Cannot open webcam")
 else:
     while True:
         # read frames
-        success, frame = cap. read()
-        # show frames in the window         
-        cv2.imshow("Captured: " , frame)
-        # check if the user press ESC or not   
-        if cv2.waitKey(1) == 27:             
+        success, frame = cap.read()
+        # show frames in the window
+        cv2.imshow("Captured: ", frame)
+        # check if the user press ESC or not
+        if cv2.waitKey(1) == 27:
             break
-# stop         
+# stop
 cap.release()
-# destroy cv2 window                              
-cv2.destroyAllWindows()                 
+# destroy cv2 window
+cv2.destroyAllWindows()
