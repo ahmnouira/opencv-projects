a48c26918c8b131930924650553edb5a7bd679ca
Ahmed Nouira <ahmnouira@gmail.com>
2026-04-07 09:04:51 +0100

    fix

--- crop/crop_start.py
+++ crop/crop_start.py
@@ -14,7 +14,7 @@ tile_w = w // nb_cols
 
 for i in range(nb_cols):
     # small adjacent for the first one
-    if FIX and i == 0:
+    if FIX and i < 3:
         crop = img[0:h, (i * tile_w) + right_m : ((i + 1) * tile_w) + left_m]
     else:
         crop = img[0:h, i * tile_w : (i + 1) * tile_w]
