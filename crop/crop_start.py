3fc3563f941ec206a5932b921aef3776a0902f56
Ahmed Nouira <ahmnouira@gmail.com>
2026-04-15 15:20:10 +0100

    updates

--- crop/crop_start.py
+++ crop/crop_start.py
@@ -1,6 +1,6 @@
 import cv2
 
-
+# the image in the root folder
 img = cv2.imread("image.jpg")
 h, w, _ = img.shape
 
@@ -9,6 +9,7 @@ right_m = 5
 left_m = 10
 FIX = False
 NAME = "tab"  # "crop_start"
+PREFIX = ""  # "_"
 
 tile_w = w // nb_cols
 
@@ -19,4 +20,4 @@ for i in range(nb_cols):
         crop = img[0:h, (i * tile_w) + right_m : ((i + 1) * tile_w) + left_m]
     else:
         crop = img[0:h, i * tile_w : (i + 1) * tile_w]
-    cv2.imwrite(f"output/{NAME}_{i + 1}.jpg", crop)
+    cv2.imwrite(f"output/{NAME}{PREFIX}{i + 1}.jpg", crop)
