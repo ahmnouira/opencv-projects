3e668fd45024c1952c391bfc46f22432ff47e449
Ahmed Nouira <ahmnouira@gmail.com>
2026-04-16 12:59:53 +0100

    feat: added fix for a specific tab icon

--- crop/crop_start.py
+++ crop/crop_start.py
@@ -1,4 +1,12 @@
 import cv2
+import shutil
+
+FIX = False
+NAME = "tab"  # "crop_start"
+PREFIX = ""  # "_"
+MOVE = True
+FIX_ONLY_LAST = True
+OUTPUT_ASSETS = "/Users/ahmnouira/Desktop/askwe/assets/tabs"
 
 # the image in the root folder
 img = cv2.imread("image.jpg")
@@ -6,11 +14,9 @@ h, w, _ = img.shape
 
 nb_cols = 5
 right_m = 5
+top_m = 0
+bottom_m = -6
 left_m = 10
-FIX = False
-NAME = "tab"  # "crop_start"
-PREFIX = ""  # "_"
-
 tile_w = w // nb_cols
 
 
@@ -18,6 +24,13 @@ for i in range(nb_cols):
     # small fixe for the first three ones
     if FIX and i < 3:
         crop = img[0:h, (i * tile_w) + right_m : ((i + 1) * tile_w) + left_m]
+
+    elif FIX_ONLY_LAST and i == nb_cols - 1:
+        crop = img[0 : h + bottom_m, i * tile_w : (i + 1) * tile_w]
     else:
         crop = img[0:h, i * tile_w : (i + 1) * tile_w]
-    cv2.imwrite(f"output/{NAME}{PREFIX}{i + 1}.jpg", crop)
+
+    file = f"output/{NAME}{PREFIX}{i + 1}.jpg"
+    cv2.imwrite(file, crop)
+    if MOVE:
+        shutil.copy(file, OUTPUT_ASSETS)
